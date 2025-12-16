2da1ba67580ef14dc901f0cba49a7583def6b0a0
Dbeauche12 <Dbeauchene>
2025-12-15 16:21:47 -0800

    next commit

---
+++ index.js
@@ -0,0 +1,27 @@
+//
+//
+//u0yyJgZj18po1tp4Z
+
+function contact(event) {
+  event.preventDefault();
+  const loading = document.querySelector(".modal__overlay--loading");
+  const success = document.querySelector(".modal__overlay--success");
+  loading.classList.remove += " modal__overlay--visible";
+  emailjs
+    .sendForm(
+      "service_9thl1up",
+      "template_znpog6g",
+      event.target,
+      "u0yyJgZj18po1tp4Z"
+    )
+    .then(() => {
+      loading.classList.remove("modal__overlay--visible");
+      success.classList += "modal__overlay--visible";
+    })
+    .catch(() => {
+      loading.classList.remove("modal__overlay--visible");
+      alert(
+        "The email service is temporarily unavailable.  Please contact me directly on dbeauche12@gmail.com"
+      );
+    });
+}
